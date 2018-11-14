13c864e9634e75de8b53d3611baeea22664eaf95
choiyk <d_pvmf@naver.com>
2018-11-14 23:42:51 +0900

    Ex4_2

--- BaekjoonAlgorithm/src/Ex4/Ex4_2.java
+++ BaekjoonAlgorithm/src/Ex4/Ex4_2.java
@@ -1,10 +1,25 @@
 package Ex4;
 
+import java.util.Scanner;
+
 public class Ex4_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int arr[] = new int[3];
+		arr[0] = scan.nextInt();
+		arr[1] = scan.nextInt();
+		arr[2] = scan.nextInt();
+		for(int j=2; j>0; j--) {
+			for(int i=0; i<j; i++) {
+				if(arr[i] > arr[i+1]) {
+					int temp = arr[i+1];
+					arr[i+1] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		System.out.println(arr[1]);
 	}
 
 }
